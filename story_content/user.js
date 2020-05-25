a4e5a2cfed87c7717c2f1ca0215155c1fca8321a
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-25 13:09:10 +0300

    ED1d1

---
+++ story_content/user.js
@@ -0,0 +1,47 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6f4Z2GigQir":
+        Script1();
+        break;
+      case "6JsK1AaYS0X":
+        Script2();
+        break;
+      case "62uXAzaXvis":
+        Script3();
+        break;
+      case "6LWihxG4tTU":
+        Script4();
+        break;
+      case "5twZjcCfBUo":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script5()
+{
+  window.print();
+}
+
